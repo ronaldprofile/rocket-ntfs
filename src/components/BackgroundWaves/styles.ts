cd8158e55a0bad835e27aab38a0eb088a7110c9d
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-03-06 00:18:46 -0300

    added dependencie framer motion

--- src/components/BackgroundWaves/styles.ts
+++ src/components/BackgroundWaves/styles.ts
@@ -2,7 +2,7 @@ import { styled } from "../../styles";
 import { backgroundWaves } from "../../assets";
 
 export const Container = styled("div", {
-  width: "100%",
-  height: "100%",
+  // width: "100%",
+  // height: "100%",
   background: `url(${backgroundWaves})`
 });
