fd2393c0fe7274939fb88c82cfdd2d80372e6d6a
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-03-09 23:45:05 -0300

    added component Tooltip

---
+++ src/components/Tooltip/index.tsx
@@ -0,0 +1,26 @@
+import { ReactNode } from "react";
+import * as TooltipPrimitive from "@radix-ui/react-tooltip";
+import { StyledContent, StyledArrow } from "./styles";
+
+export const TooltipRoot = TooltipPrimitive.Root;
+export const TooltipTrigger = TooltipPrimitive.Trigger;
+export const TooltipContent = TooltipPrimitive.Content;
+export const TooltipArrow = TooltipPrimitive.Arrow;
+
+interface TooltipProps {
+  children: ReactNode;
+  content: string;
+  side: "top" | "bottom" | "left" | "right";
+}
+
+export function Tooltip({ children, content, side }: TooltipProps) {
+  return (
+    <TooltipRoot>
+      <TooltipTrigger asChild>{children}</TooltipTrigger>
+      <StyledContent sideOffset={5} side={side}>
+        {content}
+        <StyledArrow />
+      </StyledContent>
+    </TooltipRoot>
+  );
+}
---
+++ src/components/Tooltip/styles.ts
@@ -0,0 +1,50 @@
+import { styled, keyframes } from "../../styles";
+import { TooltipContent, TooltipArrow } from ".";
+import { violet } from "@radix-ui/colors";
+
+const slideUpAndFade = keyframes({
+  "0%": { opacity: 0, transform: "translateY(2px)" },
+  "100%": { opacity: 1, transform: "translateY(0)" }
+});
+
+const slideRightAndFade = keyframes({
+  "0%": { opacity: 0, transform: "translateX(-2px)" },
+  "100%": { opacity: 1, transform: "translateX(0)" }
+});
+
+const slideDownAndFade = keyframes({
+  "0%": { opacity: 0, transform: "translateY(-2px)" },
+  "100%": { opacity: 1, transform: "translateY(0)" }
+});
+
+const slideLeftAndFade = keyframes({
+  "0%": { opacity: 0, transform: "translateX(2px)" },
+  "100%": { opacity: 1, transform: "translateX(0)" }
+});
+
+export const StyledContent = styled(TooltipContent, {
+  borderRadius: 4,
+  padding: "10px 15px",
+  fontSize: 15,
+  lineHeight: 1,
+  color: violet.violet11,
+  backgroundColor: "white",
+  boxShadow:
+    "hsl(206 22% 7% / 35%) 0px 10px 38px -10px, hsl(206 22% 7% / 20%) 0px 10px 20px -15px",
+  "@media (prefers-reduced-motion: no-preference)": {
+    animationDuration: "400ms",
+    animationTimingFunction: "cubic-bezier(0.16, 1, 0.3, 1)",
+    animationFillMode: "forwards",
+    willChange: "transform, opacity",
+    '&[data-state="delayed-open"]': {
+      '&[data-side="top"]': { animationName: slideDownAndFade },
+      '&[data-side="right"]': { animationName: slideLeftAndFade },
+      '&[data-side="bottom"]': { animationName: slideUpAndFade },
+      '&[data-side="left"]': { animationName: slideRightAndFade }
+    }
+  }
+});
+
+export const StyledArrow = styled(TooltipArrow, {
+  fill: "white"
+});
