f14b245293411ee34747c5b397374b35613953c2
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-03-15 09:43:37 -0300

    configured the media rules in the media object

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -11,5 +11,13 @@ export const { theme, styled, keyframes, globalCss } = createStitches({
       blackText: "#000",
       colorInput: "#0D0D0D"
     }
+  },
+
+  media: {
+    breakpoint1: "(max-width: 375px)",
+    breakpoint2: "(max-width: 425px)",
+    breakpoint3: "(max-width: 480px)",
+    breakpoint4: "(max-width: 768px)",
+    breakpoint5: "(max-width: 620px)"
   }
 });
