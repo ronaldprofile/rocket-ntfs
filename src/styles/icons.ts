0acdcffc54e84a3b5ba9553e85a5d817e1ea4f86
Ronald Tomaz <ronaldtmprofile@gmail.com>
2022-03-15 09:39:50 -0300

    exported icons from @radix-ui/react-icons

---
+++ src/styles/icons.ts
@@ -0,0 +1,8 @@
+import {
+  Cross1Icon,
+  HamburgerMenuIcon,
+  ArrowRightIcon,
+  ArrowTopRightIcon
+} from "@radix-ui/react-icons";
+
+export { Cross1Icon, HamburgerMenuIcon, ArrowRightIcon, ArrowTopRightIcon };
